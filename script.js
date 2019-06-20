10e28252b4b5683ddce128daf679a46976b846af
Brennan Martin <Brennan.Martin@arrow.com>
2019-06-20 14:54:17 -0600

    last commit

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 // select elements
-const btns = document.querySelectorAll('.btn');
-const screen = document.querySelector('.screen');
-const equalBtn = document.querySelector('.btn-equal');
-const clearBtn = document.querySelector('.btn-clear');
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
 
-btns.forEach(function(btn){
-  btn.addEventListener('click', function(){
-    let number = btn.getAttribute('data-num');
+btns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    let number = btn.getAttribute("data-num");
     screen.value += number;
+  });
+});
 
-  })
-})
-
-equalBtn.addEventListener('click',function(){
-  if(screen.value === ''){
-    screen.value = `Please enter a value`
+equalBtn.addEventListener("click", function() {
+  if (screen.value === "") {
+    screen.value = `Please enter a value`;
   } else {
     let value = eval(screen.value);
     screen.value = value;
   }
-})
+});
 
-clearBtn.addEventListener('click', function(){
-  screen.value = '';
-})
+clearBtn.addEventListener("click", function() {
+  screen.value = "";
+});
